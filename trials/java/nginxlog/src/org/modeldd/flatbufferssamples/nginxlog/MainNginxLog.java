94cd71ef57571d1d157e782ed5e4f55621426374
carrascoMDD <carrascomdd@gmail.com>
2018-11-27 20:35:43 +0100

    intructions run nginxlog.sh

--- trials/java/nginxlog/src/org/modeldd/flatbufferssamples/nginxlog/MainNginxLog.java
+++ trials/java/nginxlog/src/org/modeldd/flatbufferssamples/nginxlog/MainNginxLog.java
@@ -22,11 +22,10 @@ table NginxLogLine {
     agent:string;
 }
 
-Sample instances in plain text as produced by the nginx proxy (not )
+Sample instances in plain text as produced by the nginx proxy
 SpringNut.access.log.43 192.168.69.129 - - [15/Apr/2018:13:01:47 +0200] "GET / HTTP/1.1" 200 2292 "-" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
 nginx-workOflow-83.log.1 192.168.99.1 - - [06/Jun/2018:00:02:44 +0200] "GET /build/main.js HTTP/1.1" 200 287069 "http://workoflow.org/" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
 */
-
 
 
 class MainNginxLog {
